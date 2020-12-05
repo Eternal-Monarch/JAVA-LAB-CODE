af2597f3d0b3483db379bf90741d5973d28ad246
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-12-05 13:55:10 +0600

    Create 2. Write a program in Java to do arithmetic operations.java

---
+++ 2. Write a program in Java to do arithmetic operations.java
@@ -0,0 +1,16 @@
+
+class Main{ 
+    public static void main(String[] args) 
+    { 
+   
+        int num1 = 10, num2 = 20, sum ; 
+  
+
+        System.out.println("num1 = " + num1); 
+        System.out.println("num2 = " + num2); 
+  
+
+        sum = num1 + num2; 
+        System.out.println("The sum = " + sum); 
+    } 
+} 
